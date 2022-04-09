9ab570f384a6d8ad8622ee03bf29159d1f1dcfab
FMiranda97 <brugesmiranda@gmail.com>
2022-04-09 22:09:28 +0100

    add animation to transition between pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <NavBar></NavBar>
-  <router-view class="view"></router-view>
+<!--  <router-view class="view"></router-view>-->
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" :duration="{ enter: 500, leave: 800 }"/>
+    </transition>
+  </router-view>
 </template>
 
 <script>
@@ -16,6 +21,33 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+.slide-leave {
+  transform: translateX(0);
+}
+
+.slide-leave-active {
+  transition: transform 0.2s;
+}
+
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-enter {
+  transform: translateX(0);
+}
+
+.slide-enter-active {
+  transition: transform 0.2s;
+}
+
+.slide-enter-from {
+  transform: translateX(100%);
+}
+
+
+
 html, body {
   font-family: Poppins, sans-serif;
 }
